ebd33242cf4c978de766b497b8bfa16580a17610
Mohiuddin <shagorinnocent24@gmail.com>
2020-07-16 05:42:24 +0600

    first angular app

---
+++ app.js
@@ -0,0 +1,15 @@
+//    Lecture One Start     //
+
+// (function(){
+//     'use strict';
+//     angular.module('myFirstApp', [])
+//     .controller('MyFirstController', function($scope){
+//         $scope.name = "David";
+//         $scope.sayHello = function(){
+//             return "Hello Coursera!";
+//         } 
+//     });
+// }) ();
+
+//      Lecture One End     //
+
